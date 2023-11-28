57c15579d0d38f9566eeaf5473203472145c06a9
annahuangg <annahuang5668@gmail.com>
2023-11-27 22:40:26 -0600

    add search button to landing page

--- SHITWEUSING/src/components/landing/Search.jsx
+++ SHITWEUSING/src/components/landing/Search.jsx
@@ -24,6 +24,10 @@ function Search({ details }) {
       setSearchShow(true);
     }
   };
+  const handleSearch = () => {
+    // Perform any actions you want with the searchField value
+    console.log("Search button pressed. Search value:", searchField);
+  };
 
   // function searchList() {
   // 	if (searchShow) {
@@ -49,11 +53,59 @@ function Search({ details }) {
           placeholder="Enter a song, artist, or album!"
           onChange={handleChange}
         />
+        <button className="pa3 bg-light-blue br3 grow" onClick={handleSearch}>
+          Search
+        </button>
         <Category />
       </div>
-      {/* {searchList()} */}
     </section>
   );
 }
 
 export default Search;
+
+// import React, { useState } from "react";
+// import Login from "./Login";
+// import Category from "./Category";
+
+// function Search({ details }) {
+//   const [searchField, setSearchField] = useState("");
+//   const [searchShow, setSearchShow] = useState(true);
+
+//   const handleChange = (e) => {
+//     setSearchField(e.target.value);
+//     if (e.target.value === "") {
+//       setSearchShow(false);
+//     } else {
+//       setSearchShow(true);
+//     }
+//   };
+
+//   const handleSearch = () => {
+//     // Perform any actions you want with the searchField value
+//     console.log("Search button pressed. Search value:", searchField);
+//   };
+
+//   return (
+//     <section className="garamond">
+//       <Login />
+//       <div className="navy georgia ma0 grow">
+//         <h2 className="f1">Ocean of Noise</h2>
+//       </div>
+//       <div className="pa2">
+//         <input
+//           className="pa3 bb br3 grow b--none bg-lightest-blue ma3"
+//           type="search"
+//           placeholder="Enter a song, artist, or album!"
+//           onChange={handleChange}
+//         />
+//         <button className="pa3 bg-light-blue br3 grow" onClick={handleSearch}>
+//           Search
+//         </button>
+//         <Category />
+//       </div>
+//     </section>
+//   );
+// }
+
+// export default Search;
